433a2bbba52a192a6472ecc9dd0a2dc5f28ed192
vderyushev <falleretic@gmail.com>
2016-02-25 05:23:38 +1300

    Add EditableRange tests

--- ApiExamples/CSharp/Document/ExDocumentBuilder.cs
+++ ApiExamples/CSharp/Document/ExDocumentBuilder.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Drawing;
+using System.IO;
 using Aspose.Words;
 using Aspose.Words.Drawing;
 using Aspose.Words.Drawing.Charts;
@@ -1796,6 +1797,127 @@ namespace ApiExamples.Document
             Console.WriteLine(editableRange2.EditableRangeStart.Equals(edRange2Start)); // True
             Console.WriteLine(editableRange2.EditableRangeEnd.Equals(edRange2End)); // True
             //ExEnd
+        }
+
+        [Test]
+        public void EditableRangeQa()
+        {
+            Aspose.Words.Document doc = DocumentHelper.CreateDocumentFillWithDummyText();
+
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            //Get paragraphs of the current document 
+            Paragraph paraFirst = DocumentHelper.GetParagraph(doc, 0);
+            Paragraph paraSecond = DocumentHelper.GetParagraph(doc, 1);
+
+            builder.MoveTo(paraFirst);
+
+            //Add EditableRangeStart to the first paragraph
+            EditableRangeStart startRangeParaFirst = builder.StartEditableRange();
+
+            builder.Writeln("EditableRange_1_1");
+            builder.Writeln("EditableRange_1_2");
+
+            //Mark the current position as an editable range end for "startRangeParaFirst"
+            //"EndEditableRange()" closes the first created EditableRangeStart
+            builder.EndEditableRange();
+
+            //Add text to non-editable region of a document
+            builder.Writeln("NotEditableRange_1_1");
+            builder.Writeln("NotEditableRange_1_2");
+
+            builder.MoveTo(paraSecond);
+
+            //Add EditableRangeStart to the second paragraph
+            EditableRangeStart startRangeParaSecond = builder.StartEditableRange();
+
+            builder.Writeln("EditableRange_2_1");
+
+            //Mark the current position as an editable range end for "startRangeParaSecond"
+            //"EndEditableRange(EditableRangeStart)" closes EditableRangeStart which you specify in paramert
+            builder.EndEditableRange(startRangeParaSecond);
+
+            //Add text to non-editable region of a document
+            builder.Writeln("NotEditableRange_2_1");
+
+            //Sets the editor for editable range regions
+            startRangeParaFirst.EditableRange.EditorGroup = EditorType.Everyone;
+            startRangeParaSecond.EditableRange.EditorGroup = EditorType.Everyone;
+
+            //Sets that the document read only and is password-protected
+            doc.Protect(ProtectionType.ReadOnly, "123");
+
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, SaveFormat.Docx);
+
+            NodeCollection startNodes = doc.GetChildNodes(NodeType.EditableRangeStart, true);
+
+            //Assert that the document have nodes of EditableRangeStart
+            Assert.AreEqual(2, startNodes.Count);
+
+            //Assert that is the current region and structure is not broken
+            Aspose.Words.Node startRangeRun1 = startNodes[0].NextSibling;
+            Assert.AreEqual(startRangeRun1.GetText(), "EditableRange_1_1");
+
+            //Assert that is the current region and structure is not broken
+            Aspose.Words.Node startRangeRun2 = startNodes[1].NextSibling;
+            Assert.AreEqual(startRangeRun2.GetText(), "EditableRange_2_1");
+
+            //Assert that the document have nodes of EditableRangeEnd
+            NodeCollection endNodes = doc.GetChildNodes(NodeType.EditableRangeEnd, true);
+            Assert.AreEqual(2, endNodes.Count);
+
+            //Assert that is the current region and structure is not broken
+            Aspose.Words.Node endRangeRun1 = endNodes[0].NextSibling;
+            Assert.AreEqual(endRangeRun1.GetText(), "NotEditableRange_1_1");
+
+            //Assert that is the current region and structure is not broken
+            Aspose.Words.Node endRangeRun2 = endNodes[1].NextSibling;
+            Assert.AreEqual(endRangeRun2.GetText(), "NotEditableRange_2_1");
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException), ExpectedMessage = "EndEditableRange can not be called before StartEditableRange.")]
+        public void EditableRangeException()
+        {
+            Aspose.Words.Document doc = new Aspose.Words.Document();
+
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            //Is not valid structure for the current document
+            builder.EndEditableRange();
+
+            builder.StartEditableRange();
+        }
+
+        [Test]
+        public void EditableRangeNotFull()
+        {
+            Aspose.Words.Document doc = DocumentHelper.CreateDocumentFillWithDummyText();
+
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            //Add EditableRangeStart
+            EditableRangeStart startRange1 = builder.StartEditableRange();
+
+            builder.Writeln("EditableRange_1_1");
+            builder.Writeln("EditableRange_1_2");
+
+            //Sets the editor for editable range region
+            startRange1.EditableRange.EditorGroup = EditorType.Everyone;
+
+            //Sets that the document read only and is password-protected
+            doc.Protect(ProtectionType.ReadOnly, "123");
+
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, SaveFormat.Docx);
+
+            //Assert that it's not valid structure and editable ranges aren't added to the current document
+            NodeCollection startNodes = doc.GetChildNodes(NodeType.EditableRangeStart, true);
+            Assert.AreEqual(0, startNodes.Count);
+
+            NodeCollection endNodes = doc.GetChildNodes(NodeType.EditableRangeEnd, true);
+            Assert.AreEqual(0, endNodes.Count);
         }
 
 
